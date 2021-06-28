f3d613ac12b7f8fa674889f294836d3b8d25cef8
thanhchinguyenpk <nctnct1603@gmail.com>
2021-06-28 19:21:29 +0700

    add comment feature, also tag people

--- sharingvideoapp/lib/main.dart
+++ sharingvideoapp/lib/main.dart
@@ -5,6 +5,33 @@ import 'package:sharingvideoapp/pages/addvideo/addvideo_body.dart';
 import 'package:sharingvideoapp/pages/addvideo/addvideo_page.dart';
 import 'package:sharingvideoapp/pages/discover/discover_page.dart';
 import 'package:sharingvideoapp/pages/home/home_page.dart';
+//
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   cameras= await availableCameras();
+//   runApp(MyApp());
+// }
+//
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//         home: HomePage(),
+//         //initialRoute: '/home',
+//         routes: <String, WidgetBuilder>{
+//           '/home': (BuildContext context) => new HomePage(),
+//           '/add':(BuildContext context) => new AddVideoPage(),
+//           '/discover':(BuildContext context)=> new DiscoverPage(),
+//         }
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:sharingvideoapp/screen/home_screen.dart';
+
+// void main() => runApp(MyApp());
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,14 +39,16 @@ Future<void> main() async {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        home: HomePage(),
-        //initialRoute: '/home',
-        routes: <String, WidgetBuilder>{
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter tik tok',
+      //theme: ThemeData.light(),
+      // theme: ThemeData.dark(),
+      home: HomeScreen(),
+    routes: <String, WidgetBuilder>{
           '/home': (BuildContext context) => new HomePage(),
           '/add':(BuildContext context) => new AddVideoPage(),
           '/discover':(BuildContext context)=> new DiscoverPage(),
